55c27fb7ce32e051b88abb618e136f0bc4d3c63e
Radu Badea <badeaalexsandu@gmail.com>
2026-02-03 00:04:14 +0200

    Added golden-layout

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+
+import "./vendor/golden-layout/css/goldenlayout-base.css";
+import "./vendor/golden-layout/css/themes/goldenlayout-dark-theme.css";
+import "./vendor/golden-layout/css/themes/goldenlayout-light-theme.css";
+
+
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
